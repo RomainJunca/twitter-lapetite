c5b32d76ea41a6e43785aa629638d9b7679283eb
EpicKiwi <me@epickiwi.fr>
2019-03-15 15:20:29 +0100

    fix: write in file

--- get_verbs.py
+++ get_verbs.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
+with open("verbs.txt","w") as file:
+    r = requests.get(url = "https://fr.wiktionary.org/w/api.php?action=query&cmtitle=Cat%C3%A9gorie:Verbes_du_premier_groupe_en_fran%C3%A7ais&list=categorymembers&format=json&cmlimit=500&cmtype=page&cmnamespace=0").json()
+    i = 0
 
-r = requests.get(url = "https://fr.wiktionary.org/w/api.php?action=query&cmtitle=Cat%C3%A9gorie:Verbes_du_premier_groupe_en_fran%C3%A7ais&list=categorymembers&format=json&cmlimit=500&cmtype=page&cmnamespace=0").json()
-i = 0 
-
-nbr = len(r["query"]["categorymembers"])
-
-while i < nbr:
-	print(r["query"]["categorymembers"][i]["title"].encode("utf-8"))
-	i = i+1
+    nbr = len(r["query"]["categorymembers"])
+    
+    while i < nbr:
+        file.write(r["query"]["categorymembers"][i]["title"]+"\n")
+        i = i+1
