aaa72d1bf37b86edac57e8f83f7d4db5410d13e1
EpicKiwi <me@epickiwi.fr>
2019-03-17 11:20:59 +0100

    Paramètrer la petite

--- bot/publisher.py
+++ bot/publisher.py
@@ -4,14 +4,13 @@ import dateutil
 from time import sleep
 from word_manager import pick_word
 from word_manager import register_published
-
-TIME_BETWEEN_TWEETS = 60
+from settings import TWEET_INTERVAL
 
 def publish_tweet(tw):
 	lastTweets = tw.get_user_timeline()
 	lastTweetDate = datetime.strptime(lastTweets[0]["created_at"],"%a %b %d %H:%M:%S +0000 %Y") + timedelta(hours=1)
 	timeSinceLastTweet = datetime.today() - lastTweetDate
-	delay = int(TIME_BETWEEN_TWEETS - timeSinceLastTweet.total_seconds())
+	delay = int(TWEET_INTERVAL - timeSinceLastTweet.total_seconds())
 	if delay > 0:
 		print("Tweet publication in "+str(delay)+"s")
 		sleep(delay)
--- bot/settings.default.py
+++ bot/settings.default.py
@@ -1,2 +1,3 @@
 APP_KEY = "<app-key>"
 APP_SECRET = "<app-secret>"
+TWEET_INTERVAL = 3600
